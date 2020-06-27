2db1c035f2bb4e907db694311becc20b5a0eb9b6
Briana Olsen <bria.olsen@gmail.com>
2020-06-26 23:42:57 -0600

    clearing out and adding for wowishlist

--- index.js
+++ index.js
@@ -1,124 +1,112 @@
+const BlizzAPI = require('blizzapi');
+const express = require('express');
+const url = require('url');
+const path = require('path');
+const dotenv = require("dotenv");
+const { get } = require('http');
+dotenv.config();
+request = require("request");
 
-const express = require('express')
-const url = require('url')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const app = express()
+const port = 3000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  //.use(express.static("public"))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/assignments'))
-  .get('/getRate', (req, res) => {
-    const parts = url.parse(req.url, true);
-    const query = parts.query;
-    var cost = 0;
-    let params = {};
-    if ( query.mail && ['stamped', 'metered', 'flats', 'retail'].includes(query.mail) && !isNaN(parseFloat(query.weight))) {
-      var subResult = calculateRate(query.mail, query.weight)
-      params.input = prepareQuery(query.mail);
-      if (isNaN(subResult)) {
-        params.result = subResult;
-        params.input = "";
-      } else {
-        params.result = "$" + subResult.toFixed(2);
-        params.input = query.weight + " oz " + prepareQuery(query.mail);
-      }
-    }
-    res.setHeader('Content-Type', 'text/html');
-    res.render('pages/response', params)
-  })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-  
-  function prepareQuery(mailType) {
-    switch (mailType) {
-      case  "stamped":
-        return  "Letter (stamped)";
-      case  "metered":
-        return  "Letter (metered)";
-      case  "flats":
-        return  "Large Envelope (flats)";
-      case  "retail":
-        return  "First-Class Package Service - Retail";
-      default:
-        return null;
-    }
+// Found in a file named .env in the project root
+// BLIZZARD_API_CLIENT_ID="client id"
+// BLIZZARD_API_CLIENT_SECRET="client secret"
+const api = new BlizzAPI({
+    region: 'us',
+    clientId: process.env.BLIZZARD_API_CLIENT_ID,
+    clientSecret: process.env.BLIZZARD_API_CLIENT_SECRET
+});
+
+app.set('view engine', 'ejs')
+app.get('/', (req, res) =>res.render('wishlist'));
+
+
+app.get('/getItem', (req, res) => {
+  const parts = url.parse(req.url, true);
+  const query = parts.query;
+  let params = {};
+  if (query.itemNum) {
+    
+    //params = getItem(query.itemNum);
+    //params.itemNum = "3";
+    params.itemNum = query.itemNum;
+    //params.name = "" + 
+    params.name = getName(query.itemNum);
+    //params.name = "hi";
+    //params.icon = 
+    params.icon = getIcon(query.itemNum);
+    /*
+    const data =  await api.query('/data/wow/item/'+ itemNum + '?namespace=static-us&locale=en_US');
+    params.name = `${data.name}`;
+    params.id = `${data.id}`;
+    const icon =   await api.query(`/data/wow/media/item/${data.id}?namespace=static-us&locale=en_US`);
+    params.icon = `<a href="https://classic.wowhead.com/?item=${data.id}"><img src="${icon.assets[0].value}" border="2"></a><br>
+    <script>const whTooltips = {colorLinks: true, iconizeLinks: false, renameLinks: false};</script>
+    <script src="https://wow.zamimg.com/widgets/power.js"></script>
+    `;*/
   }
 
-  function calculateRate(mailType, weight) {
-  
-    switch (mailType) {
-      case "stamped":
-        return stamped(weight);
-      case "metered":
-        return metered(weight);
-      case "flats":
-        return flats(weight);
-      case "retail":
-        return retail(weight);
-      default:
-        return null;
+  res.setHeader('Content-Type', 'text/html');
+  res.render('item', params);
+
+})
+
+
+function getName(itemNum) {  
+  const data =  await api.query('/data/wow/item/'+ itemNum + '?namespace=static-us&locale=en_US');
+  return data.name;
+  /*
+  params.name = `${data.name}`;
+  params.id = `${data.id}`;
+  const icon =   await api.query(`/data/wow/media/item/${data.id}?namespace=static-us&locale=en_US`);
+  params.icon = `<a href="https://classic.wowhead.com/?item=${data.id}"><img src="${icon.assets[0].value}" border="2"></a><br>
+  <script>const whTooltips = {colorLinks: true, iconizeLinks: false, renameLinks: false};</script>
+  <script src="https://wow.zamimg.com/widgets/power.js"></script>
+  `;
+
+  return params;*/
+}
+
+async function getIcon(itemNum) {
+  const iconAPI =   await api.query(`/data/wow/media/item/` + itemNum + `?namespace=static-us&locale=en_US`);
+
+  let icon = `<a href="https://classic.wowhead.com/?item=` + itemNum + `"><img src="${icon.assets[0].value}" border="2"></a><br>
+  <script>const whTooltips = {colorLinks: true, iconizeLinks: false, renameLinks: false};</script>
+  <script src="https://wow.zamimg.com/widgets/power.js"></script>
+  `;
+
+  return icon;
+}
+
+app.get('/get', async (req, res) => {
+    try {
+        const data = await api.query(`/data/wow/item/19019?namespace=static-us&locale=en_US`);
+        console.log(data);
+        console.log("ITEM CLASS::: " + JSON.stringify(data.preview_item.stats.type))
+
+        const icon = await api.query(`/data/wow/media/item/${data.id}?namespace=static-us&locale=en_US`);
+        //console.log(icon);
+
+        doc = `
+        <h3>${data.name}</h3>
+        <a href="https://classic.wowhead.com/?item=${data.id}"><img src="${icon.assets[0].value}" border="2"></a><br>
+        <script>const whTooltips = {colorLinks: true, iconizeLinks: false, renameLinks: false};</script>
+        <script src="https://wow.zamimg.com/widgets/power.js"></script>
+        `;
+
+        //res.send(doc);
+        console.log(doc);
+
+        //=> '<!doctype html> ...'
+    } catch (error) {
+        res.status(error.status).send(error.response.body);
+        //=> 'Internal server error ...'
     }
-  }
-  
-  function stamped(weight) {
+})
 
-    if (weight >= 3.5) {
-      return "Too heavy: consider using large envelope";
-    }
 
-    var rate = .55;
-    for (var i = 1; i < weight; i++) {
-      rate += .15;
-    }
-    return rate;
-  }
-  
-  function metered(weight) {
-
-    if (weight > 3.5) {
-      return "Too heavy: consider using large envelope";
-    }
-
-    var rate = .50;
-    for (var i = 1; i < weight; i++) {
-      rate += .15;
-    }
-    return rate;
-  }
-  
-  function flats(weight) {
-
-    if (weight > 13) {
-      return "Too heavy: considered using package"
-    }
-
-    var rate = 1;
-    for (var i = 1; i < weight; i++) {
-      rate += .2;
-    }
-    return rate;
-  }
-
-  function retail(weight) {
-
-    if (weight > 13) {
-      return "Too heavy: unable to calculate price"
-    }
-
-    var rate = 3.80;
-
-    if (weight > 4) {
-      rate += .8;
-    }
-
-    if (weight > 8) {
-      rate += .7;
-    }
-
-    if(weight > 12) {
-      rate += .6;
-    }
-    return rate;
-  }
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+})
