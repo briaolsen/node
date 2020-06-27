23aaf28ff38b07788d1eba81da78e69830ab9c6d
Briana Olsen <bria.olsen@gmail.com>
2020-06-27 01:31:35 -0600

    database

--- index.js
+++ index.js
@@ -1,71 +1,74 @@
-const BlizzAPI = require('blizzapi');
-const express = require('express');
-const url = require('url');
-const path = require('path');
+const BlizzAPI = require("blizzapi");
+const express = require("express");
+const url = require("url");
+const path = require("path");
 const dotenv = require("dotenv");
-const { get } = require('http');
+const { get } = require("http");
 dotenv.config();
 request = require("request");
 
-const app = express()
-const port = process.env.PORT || 5000
+const app = express();
+const port = process.env.PORT || 5000;
 
 const api = new BlizzAPI({
-    region: 'us',
-    clientId: process.env.BLIZZARD_API_CLIENT_ID,
-    clientSecret: process.env.BLIZZARD_API_CLIENT_SECRET
+  region: "us",
+  clientId: process.env.BLIZZARD_API_CLIENT_ID,
+  clientSecret: process.env.BLIZZARD_API_CLIENT_SECRET,
 });
 
-app.set('view engine', 'ejs')
-app.get('/', (req, res) =>res.render('wishlist'));
+app.set("view engine", "ejs");
+app.get("/", (req, res) => res.render("wishlist"));
 
-
-app.get('/getItem', async (req, res) => {
-  const parts = url.parse(req.url, true);
-  const query = parts.query;
-  let params = {};
-  if (query.itemNum) {
-        
-    const data =  await api.query('/data/wow/item/'+ query.itemNum + '?namespace=static-us&locale=en_US');
-    params.name = data.name;
-    params.id = data.id;
-    const icon =   await api.query(`/data/wow/media/item/${data.id}?namespace=static-us&locale=en_US`);
-    params.icon = icon.assets[0].value;
-  }
-
-  res.setHeader('Content-Type', 'text/html');
-  res.render('item', params);
-
-})
-
-/*
-app.get('/get', async (req, res) => {
-    try {
-        const data = await api.query(`/data/wow/item/19019?namespace=static-us&locale=en_US`);
-        console.log(data);
-        console.log("ITEM CLASS::: " + JSON.stringify(data.preview_item.stats.type))
-
-        const icon = await api.query(`/data/wow/media/item/${data.id}?namespace=static-us&locale=en_US`);
-        //console.log(icon);
-
-        doc = `
-        <h3>${data.name}</h3>
-        <a href="https://classic.wowhead.com/?item=${data.id}"><img src="${icon.assets[0].value}" border="2"></a><br>
-        <script>const whTooltips = {colorLinks: true, iconizeLinks: false, renameLinks: false};</script>
-        <script src="https://wow.zamimg.com/widgets/power.js"></script>
-        `;
-
-        //res.send(doc);
-        console.log(doc);
-
-        //=> '<!doctype html> ...'
-    } catch (error) {
-        res.status(error.status).send(error.response.body);
-        //=> 'Internal server error ...'
+app.get("/getItem", async (req, res) => {
+  
+    const parts = url.parse(req.url, true);
+    const query = parts.query;
+    let params = {};
+    if (query.itemNum) {
+      const data = await api.query(
+        "/data/wow/item/" + query.itemNum + "?namespace=static-us&locale=en_US"
+      );
+      params.name = data.name;
+      params.id = data.id;
+      const icon = await api.query(
+        `/data/wow/media/item/${data.id}?namespace=static-us&locale=en_US`
+      );
+      params.icon = icon.assets[0].value;
+      console.log(data);
     }
-})*/
+ 
+  res.setHeader("Content-Type", "text/html");
+  res.render("item", params);
+});
 
+
+
+const { Pool } = require("pg");
+const connectionString = process.env.DATABASE_URL || "postgres://zvscnytvvthqcg:3976c888faa915b2940ddfe8223991fcc926aecaa2cc9a4a6877022af6b357a4@ec2-50-17-90-177.compute-1.amazonaws.com:5432/dcn3hmphnedhed";
+const pool = new Pool({
+    connectionString: connectionString,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+app.get("/insertItem", (req, res) => {
+
+  let params = {};
+  params.id = req.query.id;
+  params.name = req.query.name;
+  params.icon = req.query.icon;
+  
+  const sql = 'INSERT INTO item (id, name, icon) VALUES (?,?,?)';
+
+  pool.query(sql, params, function(err, result) {
+    if (err) {
+			console.log("Error in query: ")
+			console.log(err);
+        }
+  })
+})
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-})
+  console.log(`App listening at http://localhost:${port}`);
+});
