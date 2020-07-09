3f3efe2823cc7785bfa60e24363e7f55ce22e82d
Briana Olsen <bria.olsen@gmail.com>
2020-07-09 03:49:58 -0600

    Authentication pw/username

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const { get } = require("http");
 const bodyParser = require("body-parser");
 dotenv.config();
 request = require("request");
+var session = require("express-session");
+var FileStore = require("session-file-store")(session);
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,46 +21,170 @@ const api = new BlizzAPI({
   clientSecret: process.env.BLIZZARD_API_CLIENT_SECRET,
 });
 
+// INSERT ITEM AFTER SEARCH
+const { Pool } = require("pg");
+const connectionString =
+  process.env.DATABASE_URL ||
+  "postgres://zvscnytvvthqcg:3976c888faa915b2940ddfe8223991fcc926aecaa2cc9a4a6877022af6b357a4@ec2-50-17-90-177.compute-1.amazonaws.com:5432/dcn3hmphnedhed";
+const pool = new Pool({
+  connectionString: connectionString,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
 const slots = [
-  {slot: 'Head',      name: ['Head']},
-  {slot: 'Neck',      name: ['Neck']},
-  {slot: 'Shoulder',  name: ['Shoulder']},
-  {slot: 'Back',      name: ['Back']},
-  {slot: 'Chest',     name: ['Chest']},
-  {slot: 'Wrist',     name: ['Wrist']},
-  {slot: 'Hands',     name: ['Hands']},
-  {slot: 'Waist',     name: ['Waist']},
-  {slot: 'Legs',      name: ['Legs']},
-  {slot: 'Feet',      name: ['Feet']},
-  {slot: 'Finger1',   name: ['Finger']},
-  {slot: 'Finger2',   name: ['Finger']},
-  {slot: 'Trinket1',  name: ['Trinket']},
-  {slot: 'Trinket2',  name: ['Trinket']},
-  {slot: 'Mainhand',  name: ['Two-Hand', 'One-Hand','Main Hand']},
-  {slot: 'Offhand',   name: ['One-Hand','Held in Off-hand','Off Hand']},
-  {slot: 'Ranged',    name: ['RANGEDRIGHT','Relic']},
-  {slot: 'Quiver',    name: ['Bag']}
-]
+  { slot: "Head", name: ["Head"] },
+  { slot: "Neck", name: ["Neck"] },
+  { slot: "Shoulder", name: ["Shoulder"] },
+  { slot: "Back", name: ["Back"] },
+  { slot: "Chest", name: ["Chest"] },
+  { slot: "Wrist", name: ["Wrist"] },
+  { slot: "Hands", name: ["Hands"] },
+  { slot: "Waist", name: ["Waist"] },
+  { slot: "Legs", name: ["Legs"] },
+  { slot: "Feet", name: ["Feet"] },
+  { slot: "Finger1", name: ["Finger"] },
+  { slot: "Finger2", name: ["Finger"] },
+  { slot: "Trinket1", name: ["Trinket"] },
+  { slot: "Trinket2", name: ["Trinket"] },
+  { slot: "Mainhand", name: ["Two-Hand", "One-Hand", "Main Hand"] },
+  { slot: "Offhand", name: ["One-Hand", "Held in Off-hand", "Off Hand"] },
+  { slot: "Ranged", name: ["RANGEDRIGHT", "Relic"] },
+  { slot: "Quiver", name: ["Bag"] },
+];
+
+/**
+ * Setup the session store
+ */
+app.use(
+  session({
+    name: "server-session-cookie-id",
+    secret: "SDfimf3409masdc0923imasdfmp",
+    saveUninitialized: true,
+    resave: true,
+    store: new FileStore(),
+  })
+);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + '/public'));
-app.get("/", (req, res) => res.render("home"));
-app.get("/wishlist", (req, res) => res.render("wishlist", {slots: slots}));
-app.get("/database", (req, res) => res.render("database"));
+app.use(express.static(__dirname + "/public"));
 
-app.get("/login", (req, res)=> {
+/**
+ * Create a logger middleware
+ */
+var verifyLogin = function (req, res, next) {
+  if (req.session && req.session.username) {
+    console.log("Logged in as " + req.session.username);
+    next();
+  } else {
+    res.status(401);
+    res.json({ success: false, message: "unauthorized" });
+  }
+};
 
-    // login user and import wishlist 
+app.get("/", (req, res) => {
 
-    //res.setHeader("Content-Type", "text/html");
-    res.render("/wishlist");
-
+  if (req.session && req.session.username) {
+    console.log(req.session.username);
+    res.render("home", { page: "home", user: ''+req.session.username, loginStatus: true });
+  }
+  else {
+    res.render("home", { page: "home", loginStatus: false });
+  }
+  
 });
 
-// SEARCH ITEM BY ID
+/**
+ * USER LOGOUT
+ */
+app.post('/logout', function(req, res) {
+  if (req.session && req.session.username) {
+      delete req.session.username;
+      res.render('home', { page: "home", loginStatus: false });
+  } else {
+      res.end();
+  }
+});
+
+/**
+ * USER LOGIN
+ */
+app.post("/login", (req, results) => {
+  
+  const username = req.body.username;
+  const password = req.body.password;
+  console.log("attempting login...");
+  let loginStatus = false;
+
+  const sql = `SELECT * FROM users WHERE username = $1`;
+
+  pool.query(sql, [username], function (err, result) {
+    if (err) {
+      console.log("User not found");
+      console.log(err);
+      return false;
+    } else {        
+        bcrypt.compare(password, result.rows[0].hash, function(err, res) {
+          loginStatus = res;
+          if (loginStatus) {
+            req.session.username = username;
+          }
+          results.json({ success: loginStatus, username: req.session.username });
+        })
+      }
+    });    
+  });
+
+/**
+ * CREATE AN ACCOUNT
+ */
+app.post("/createAccount", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  console.log("Username: " + username + " Password: " + password);
+  let loginStatus = false;
+  let message = "Error creating account";
+
+  bcrypt.hash(password, saltRounds, (err, hash) => {
+    
+    const sql = `INSERT INTO users VALUES ($1, $2)`;
+
+    pool.query(sql, [username, hash], function (err, result) {
+
+      if (err) {
+        console.log("Unable to create account");
+        console.log(err);
+        message = "Error in username or password";
+      } else {
+        loginStatus = true;
+        req.session.username = username;
+        message = "User " + req.session.username + " created.";
+      }
+      res.json({ success: loginStatus, message: message, username: req.session.username });
+    });
+  });
+});
+
+/**
+ * GET WISHLIST PAGE
+ */
+app.get("/wishlist", verifyLogin, (req, res) =>
+  res.render("wishlist", { slots: slots, page: "wishlist", user: req.session.username })
+);
+
+/**
+ * GET DATABASE PAGE
+ */
+app.get("/database", verifyLogin, (req, res) =>
+  res.render("database", { page: "database" })
+);
+
+/**
+ * SEARCH ITEM BY ID USING BLIZZARD API
+ */
 app.get("/getItemById", async (req, res) => {
   const parts = url.parse(req.url, true);
   console.log(req.query);
@@ -84,29 +212,18 @@ app.get("/getItemById", async (req, res) => {
       );
       params.icon = icon.assets[0].value;
 
-      res.json({ status: 'success', data: params });
+      res.json({ status: "success", data: params });
     } else {
-      res.json({ status: 'error', message: 'Please enter an id' });
+      res.json({ status: "error", message: "Please enter an id" });
     }
   } catch {
-    res.json({ status: 'error', message: `Not a valid id: ${id}` });
+    res.json({ status: "error", message: `Not a valid id: ${id}` });
   }
 });
 
-// INSERT ITEM AFTER SEARCH
-const { Pool } = require("pg");
-const connectionString =
-  process.env.DATABASE_URL ||
-  "postgres://zvscnytvvthqcg:3976c888faa915b2940ddfe8223991fcc926aecaa2cc9a4a6877022af6b357a4@ec2-50-17-90-177.compute-1.amazonaws.com:5432/dcn3hmphnedhed";
-const pool = new Pool({
-  connectionString: connectionString,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
-
-
-// RETURN ITEM USING NAME
+/**
+ * GET ITEM BY NAME FROM DATABASE
+ */
 app.get("/getItemByName", (req, res) => {
   const parts = url.parse(req.url, true);
   const query = parts.query;
@@ -123,7 +240,9 @@ app.get("/getItemByName", (req, res) => {
   });
 });
 
-// INSERT ITEM INTO DATABASE
+/**
+ * INSERT ITEM INTO DATABASE
+ */
 app.post("/insertItemDatabase", (req, res) => {
   console.log("Item is being inserted...");
   console.log(req.body);
@@ -134,18 +253,34 @@ app.post("/insertItemDatabase", (req, res) => {
   //const sql = `INSERT INTO items2 VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`;
   const sql = `INSERT INTO items VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`;
 
-  pool.query(sql, [data.id, data.name, data.quality, data.itemClass, data.itemSubclass, data.inventoryType, data.inventoryName, data.icon/*, data.slot*/], function (err, result) {
-    if (err) {
-      console.log("Error in query: ");
-      console.log(err);
-    } else {
-      res.redirect("database");
+  pool.query(
+    sql,
+    [
+      data.id,
+      data.name,
+      data.quality,
+      data.itemClass,
+      data.itemSubclass,
+      data.inventoryType,
+      data.inventoryName,
+      data.icon /*, data.slot*/,
+    ],
+    function (err, result) {
+      if (err) {
+        console.log("Error in query: ");
+        console.log(err);
+      } else {
+        res.redirect("database");
+      }
     }
-  });
+  );
 });
 
+/**
+ * FIND THE SLOT THE ITEM SHOULD BE INSERTED INTO
+ */
 function getSlot(type, names) {
-  for(var i = 0; i < slots.length; i++) {
+  for (var i = 0; i < slots.length; i++) {
     if (slots[i].name.includes(type) || slots[i].name.includes(names)) {
       return slots[i].slot;
     }
@@ -153,27 +288,27 @@ function getSlot(type, names) {
   return "none";
 }
 
-// SHOW DATABASE
+/**
+ * RETURN THE ITEM DATABASE
+ */
 app.get("/getDatabase", (req, res) => {
-
-  const sql = 'SELECT * FROM items';
+  const sql = "SELECT * FROM items";
   //const sql = 'SELECT * FROM items2';
   pool.query(sql, function (err, result) {
-
     if (err) {
-      console.log("Error in query: ")
+      console.log("Error in query: ");
       console.log(err);
     }
 
     console.log(result.rows);
     res.json(result.rows);
-    //res.setHeader("Content-Type", "text/html");
-    //res.render("database");
   });
 });
 
-// INSERT ITEM INTO WISHLIST
 
+/**
+ * INSERT ITEM INTO WISHLIST
+ */
 app.post("/insertItemWishlist", (req, res) => {
   console.log("Item is being inserted into wishlist...");
   console.log(req.body);
@@ -191,18 +326,30 @@ app.post("/insertItemWishlist", (req, res) => {
 
   const sql = `INSERT INTO wishlist VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`;
 
-  pool.query(sql, [data.id, data.name, data.quality, data.itemClass, data.itemSubclass, data.inventoryType, data.inventoryName, data.icon], function (err, result) {
-    if (err) {
-      console.log("Error in query: ");
-      console.log(err);
-    } else {
-      res.redirect("database");
+  pool.query(
+    sql,
+    [
+      data.id,
+      data.name,
+      data.quality,
+      data.itemClass,
+      data.itemSubclass,
+      data.inventoryType,
+      data.inventoryName,
+      data.icon,
+    ],
+    function (err, result) {
+      if (err) {
+        console.log("Error in query: ");
+        console.log(err);
+      } else {
+        res.redirect("database");
+      }
     }
-  });
+  );
 });
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
-
 
